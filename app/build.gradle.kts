e6adeb2335ad9ebf8b392211370ecc23b1976943
Amaury PM <amaury.pema@gmail.com>
2025-03-29 07:11:14 -0600

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.amaurypm.videogamesrf"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.amaurypm.videogamesrf"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures{
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    //Para retrofit y Gson
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //Adicional para el interceptor
+    implementation(libs.logging.interceptor)
+
+    //Glide y Picasso
+    implementation(libs.glide)
+    implementation(libs.picasso)
+
+    //Para las corrutinas con alcance lifecycle (opcional)
+    //implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
+    //Imágenes con bordes redondeados
+    implementation(libs.roundedimageview)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
